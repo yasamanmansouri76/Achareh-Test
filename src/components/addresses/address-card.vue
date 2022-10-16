74d1130ce9f6ab2a275c57b02f51b436480ebad4
Yasaman Mansouri <mansouri@sabacell>
2022-10-16 13:48:14 +0330

    Add address list ui

--- src/components/addresses/address-card.vue
+++ src/components/addresses/address-card.vue
@@ -1,5 +1,58 @@
 <template>
-  <b-col class="p-3"> dfgdgdfg </b-col>
+  <b-col
+    cols="12"
+    class="p-3 bg-white float-lg-right shadow-lg-sm rounded mb-3"
+  >
+    <b-col cols="12" class="d-flex justify-content-between mb-2 d-lg-none">
+      <span class="text-dark-grey txt-mid"> نام و نام‌خانوادگی </span>
+      <span class="txt-dark txt-mid font-weight-bold"> یاسمن منصوری </span>
+    </b-col>
+    <b-col
+      lg="4"
+      class="d-none justify-content-between mb-2 float-lg-right d-lg-flex flex-lg-column p-lg-4"
+    >
+      <span class="text-dark-grey txt-mid mb-lg-3"> نام </span>
+      <span class="txt-dark txt-mid font-weight-bold"> یاسمن </span>
+    </b-col>
+    <b-col
+      lg="4"
+      class="d-none justify-content-between mb-2 float-lg-right d-lg-flex flex-lg-column p-lg-4"
+    >
+      <span class="text-dark-grey txt-mid mb-lg-3"> نام‌خانوادگی </span>
+      <span class="txt-dark txt-mid font-weight-bold"> منصوری </span>
+    </b-col>
+    <b-col
+      cols="12"
+      lg="4"
+      class="d-flex justify-content-between mb-2 float-lg-right flex-lg-column p-lg-4"
+    >
+      <span class="text-dark-grey txt-mid mb-lg-3"> جنسیت </span>
+      <span class="txt-dark txt-mid font-weight-bold"> خانم</span>
+    </b-col>
+    <b-col
+      cols="12"
+      lg="4"
+      class="d-flex justify-content-between mb-2 float-lg-right flex-lg-column p-lg-4"
+    >
+      <span class="text-dark-grey txt-mid mb-lg-3"> شماره تلفن همراه </span>
+      <span class="txt-dark txt-mid font-weight-bold"> 09017931515 </span>
+    </b-col>
+    <b-col
+      cols="12"
+      lg="4"
+      class="d-flex justify-content-between mb-2 float-lg-right flex-lg-column p-lg-4"
+    >
+      <span class="text-dark-grey txt-mid mb-lg-3"> شماره تلفن ثابت </span>
+      <span class="txt-dark txt-mid font-weight-bold"> - </span>
+    </b-col>
+    <hr class="d-lg-none" />
+    <b-col cols="12" lg="4" class="d-flex flex-column float-lg-right p-lg-4">
+      <span class="text-dark-grey txt-mid mb-2 mb-lg-3"> آدرس </span>
+      <span class="txt-dark txt-mid font-weight-bold">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+      </span>
+    </b-col>
+  </b-col>
 </template>
 
 <script>
--- src/views/addresses/index.vue
+++ src/views/addresses/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <b-col class="p-3">
-    <h6>آدرس‌ها و مشخصات</h6>
+  <b-col cols="12" class="p-3 px-lg-large pt-lg-5">
+    <h6 class="mb-3 txt-dark">آدرس‌ها و مشخصات</h6>
+    <addresse-card />
     <addresse-card />
   </b-col>
 </template>
