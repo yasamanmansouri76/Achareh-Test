8e3a073add861c940ad78dd3c0db27f206dcad5b
Yasaman Mansouri <mansouri@sabacell>
2022-10-16 11:01:36 +0330

    Add dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue } from "bootstrap-vue";
+import VeeValidate from "vee-validate";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VeeValidate, {
+  inject: true,
+  fieldsBagName: "veeFields",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
